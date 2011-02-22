fabcff1e3925f2074566187bb9f6a186c8deee6f
Jim <openbip@gmail.com>
2011-02-22 08:57:05 -0500

    Initial rails-generated content.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransletteRails::Application.config.secret_token = '15cc39621e9e00c9d45a553aa8d08c2d51f2287594c4f79b28d57e3b41ad58b5e32d0a5c5547881c510fa288eda65f9e2da8ff835a0d7d1acb9a4904c33bdc95'
